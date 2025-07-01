18b09b5a6c5e5a0e1e3bf6221c984a90c2cef29b
bp <root@mythidas.com>
2025-07-01 14:24:36 -0500

    impl home page metrics

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,1 +1,43 @@
-export default async function Home() {}
+'use client';
+
+import Loader from '@/components/common/Loader';
+import SourceMetricsAggregatedTable from '@/components/domains/metrics/tables/SourceMetricsAggregatedTable';
+import { useAsync } from '@/hooks/useAsync';
+import { getSourceIntegrationsView } from '@/services/integrations';
+
+export default function Home() {
+  const {
+    data: { integrations },
+    isLoading,
+  } = useAsync({
+    initial: { integrations: [] },
+    fetcher: async () => {
+      const integrations = await getSourceIntegrationsView();
+      if (!integrations.ok) {
+        throw integrations.error.message;
+      }
+
+      return {
+        integrations: integrations.data,
+      };
+    },
+    deps: [],
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      {integrations.map((integration) => {
+        return (
+          <div key={integration.id} className="flex flex-col gap-4">
+            <h1>{integration.source_name}</h1>
+            <SourceMetricsAggregatedTable sourceId={integration.source_id!} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
