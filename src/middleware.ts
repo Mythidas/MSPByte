96319bb3e4ae9f596717ab19acb680d52f69f169
bp <root@mythidas.com>
2025-07-08 12:06:21 -0500

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import { updateSession } from 'packages/db/middleware';
 
 export async function middleware(request: NextRequest) {
-  const isCron = request.headers.get('x-vercel-cron') === '1';
-  if (isCron) return NextResponse.next();
-
   return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
