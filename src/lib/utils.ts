19ebcf29b4653b62d6567720f8979be4d1663351
bp <root@mythidas.com>
2025-07-22 20:08:14 -0500

    Change timer to log in seconds

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -68,18 +68,18 @@ export class Timer {
   }
 
   end(name: string) {
-    this.timestamps[name] = (new Date().getTime() - this.timestamps[name]) / (1000 * 60);
+    this.timestamps[name] = (new Date().getTime() - this.timestamps[name]) / 1000;
     this.total += this.timestamps[name];
   }
 
   summary() {
     if (!this.on) return;
 
-    const duration = ((new Date().getTime() - this.start.getTime()) / (1000 * 60)).toFixed(2);
-    console.log(`[${this.name}] ${duration}m elapsed`);
+    const duration = ((new Date().getTime() - this.start.getTime()) / 1000).toFixed(2);
+    console.log(`[${this.name}] ${duration}s elapsed`);
     for (const [key, value] of Object.entries(this.timestamps)) {
       console.log(
-        `[${key}] ${value.toFixed(2)}m duration (${((value / this.total) * 100).toFixed(2)}%)`
+        `[${key}] ${value.toFixed(2)}s duration (${((value / this.total) * 100).toFixed(2)}%)`
       );
     }
   }
